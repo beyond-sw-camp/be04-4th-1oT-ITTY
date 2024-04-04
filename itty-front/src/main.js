210ef65fb4c8d2ed4addb1968e4c758ab735a0da
jhlee <jun73045928@gmail.com>
2024-04-04 12:33:24 +0900

    Init: "Vue 프로젝트 초기 세팅"
    router dependency 추가
    기본 assets, components 제거

--- itty-front/src/main.js
+++ itty-front/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/router.js';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
