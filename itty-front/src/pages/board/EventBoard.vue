d9f3cd840384ee4cff49a796053651587e1c8845
mini-xi <gam1205@naver.com>
2024-04-11 12:24:54 +0900

    Feat: "자유게시판 및 이벤트게시판 추가"
    - 자유 게시판 수정
    - 이벤트 게시판(Beta)

---
+++ itty-front/src/pages/board/EventBoard.vue
@@ -0,0 +1,18 @@
+<template>
+    
+    <MainHeader/>
+     <img src="@/pages/board/img/event-test.png" style="margin-top: 150px;margin-left: 22%;margin-bottom: 20px;">
+    <MainFooter/>
+  </template>
+  
+  <script setup>
+
+  import MainHeader from '@/components/Header.vue';
+  import MainFooter from '@/components/Footer.vue';
+
+  </script>
+  
+  <style>
+
+  </style>
+  
