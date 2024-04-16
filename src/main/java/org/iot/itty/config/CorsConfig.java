16dfabc85f1c92fbef3f4eaf9f4a3ada4b3a2b78
bucky1005 <null00007@naver.com>
2024-04-16 14:52:29 +0900

    refactor: "security CORS 변경"
    security CORS 포트 번호 30001 -> 5173 변경

--- src/main/java/org/iot/itty/config/CorsConfig.java
+++ src/main/java/org/iot/itty/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 		CorsConfiguration config = new CorsConfiguration();
 
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of("http://localhost:5173"));
+		config.setAllowedOrigins(List.of("http://localhost:30002"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setExposedHeaders(List.of("*"));
